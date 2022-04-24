f4b1f7434066ebf99753e865bf912c6184b58aad
tukanoidd <tukanoid@outlook.com>
2022-04-24 18:29:00 +0200

    started macro, just saving

--- tmath/src/lib.rs
+++ tmath/src/lib.rs
@@ -1,8 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use paste::paste;
+
+use tmath_macros::Vector;
+
+macro_rules! declare_vector_variants {
+    (
+        $len:literal;
+        [$(
+            ($($variant_name:ident, $variant_ty:ty),+)
+        ),+]
+    ) => {
+        paste! {
+            $($(
+                #[derive(Default, Copy, Clone, PartialEq, Vector)]
+                pub struct [< Vector $len $variant_name >]([$variant_ty; $len]);
+            )*)*
+        }
+    };
 }
+
+macro_rules! declare_vectors {
+    ($($len:literal),*) => {
+        paste! {
+            $(
+                #[derive(Default, Copy, Clone, PartialEq, Vector)]
+                pub struct [< Vector $len >]([f32; $len]);
+
+                declare_vector_variants!(
+                    $len;
+                    [(D, f64), (I, i32), (L, i64), (U, u32), (UL, u64)]
+                );
+            )*
+        }
+    };
+}
+
+declare_vectors!(2, 3, 4);
--- tmath_macros/src/lib.rs
+++ tmath_macros/src/lib.rs
@@ -1,8 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use proc_macro::TokenStream;
+use proc_macro2::Ident;
+use quote::quote;
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsUnnamed, Type, TypeArray,
+};
+
+// ----- Derives START -----
+// ----- Vector START -----
+#[proc_macro_derive(Vector)]
+pub fn derive_vector(input: TokenStream) -> TokenStream {
+    let DeriveInput {
+        ident: struct_name,
+        data,
+        ..
+    } = parse_macro_input!(input);
+
+    match data {
+        Data::Struct(DataStruct { fields, .. }) => match fields {
+            Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => {
+                let arr_field = unnamed.first().unwrap();
+                parse_array_vector(&struct_name, arr_field)
+            }
+            Fields::Named(_) => panic!("Named structs are not yet supported"),
+            Fields::Unit => {
+                panic!("Unit structs are not permitted, it has to contain data to work with")
+            }
+        },
+        _ => panic!("Only structs are permitted"),
     }
 }
+
+fn parse_array_vector(struct_name: &Ident, arr_field: &Field) -> TokenStream {
+    match arr_field.ty {
+        Type::Array(TypeArray {
+            elem: var_ty, len, ..
+        }) => {
+            // TODO(tukanoidd): implement
+            (quote! {}).into()
+        }
+        _ => panic!("First struct member has to be an array"),
+    }
+}
+// ----- Vector END -----
+// ----- Derives END -----
